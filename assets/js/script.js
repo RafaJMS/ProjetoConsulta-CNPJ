4a0127b545bfb7d63db00c48e94f2754b07c1fb1
Rafael Mecenas <rafaeljmecenas1@gmail.com>
2023-12-20 17:15:41 -0300

    Inicio de Projeto

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+function validarCNPJ(CNPJ)
+{
+    $.ajax({
+        'url':'https://receitaws.com.br/v1/cnpj/' + CNPJ.replace(/[^0-9]/g,''),
+        'type':'GET',
+        'dataType':'jsonp',
+        'success':function(dado)
+        {
+            if(dado.nome == undefined || ){
+                alert(dado.status + " " +dado.message)
+            }else{
+            document.getElementById('razao-social').value = dado.nome;
+            document.getElementById('nome-fantasia').value = dado.fantasia
+            document.getElementById('data-abertura').value = dado.abertura
+            document.getElementById('status-empresa').value = dado.status
+            document.getElementById('endereco').value = dado.logradouro
+            document.getElementById('cidade-estado').value = `${dado.municipio} - ${dado.uf}`
+            document.getElementById('telefone').value = dado.telefone
+            console.log(dado)
+        }
+    }
+
+
+    })
+}
+//
